0b89dc902d1a6b8c950889e236f96acc286601f4
Lucas Combs <l-combs@hotmail.com>
2019-12-15 13:46:16 -0500

    NOGH: Generate Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace discord_SMART_monitoring_hook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
